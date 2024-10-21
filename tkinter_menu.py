d4dfced5ac846e0c5e00943d4370fd8308d288cb
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-10-21 18:46:58 -0300

    Update tkinter_menu.py

--- tkinter_menu.py
+++ tkinter_menu.py
@@ -11,8 +11,7 @@ aplicativo.configure(background='#ddf')
 
 barraMenu = Menu(aplicativo)
 
-
-menuComandos = Menu(barraMenu)
+menuComandos = Menu(barraMenu, tearoff= 0)
 menuComandos.add_command(label='Novo', command=vazio)
 menuComandos.add_command(label='Buscar', command=vazio)
 menuComandos.add_command(label='Excluir', command=vazio)
@@ -20,8 +19,13 @@ menuComandos.add_separator()
 menuComandos.add_command(label='Sair', command=aplicativo.quit)
 barraMenu.add_cascade(label='Comandos', menu=menuComandos)
 
-menuManutencao= Menu(aplicativo)
+menuManutencao= Menu(aplicativo, tearoff= 0)
 menuManutencao.add_command(label= 'Registro', command=vazio)
-menuManutencao.add_cascade(label= 'Manutenção', menu= menuManutencao)
+barraMenu.add_cascade(label= 'Manutenção', menu= menuManutencao)
+
+menuRelacionado= Menu(barraMenu, tearoff= 0)
+menuRelacionado.add_command(label= 'Arquivos', command= vazio)
+barraMenu.add_cascade(label= 'Relacionado', menu= menuRelacionado)
+
 aplicativo.config(menu=barraMenu)
 aplicativo.mainloop()
